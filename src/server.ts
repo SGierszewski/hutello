b4e7aa68e1190082e790e90121018f9906d13ebf
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-10 10:13:36 +0200

    :ambulance: Forward client routing in production

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import path from "path";
+
 const app = express();
 const { PORT = 3000 } = process.env;
 
@@ -10,6 +12,11 @@ app.use("/storybook", express.static("dist/storybook"));
 
 // Serve app production bundle
 app.use(express.static("dist/app"));
+
+// Handle client routing, return all requests to the app
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "app/index.html"));
+});
 
 app.get("/", (_req, res) => {
   res.send("Hello World!");
