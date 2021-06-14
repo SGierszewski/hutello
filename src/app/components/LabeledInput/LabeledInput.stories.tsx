0929540c254b9fc8b4b7c4fb937eff25acadce44
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-14 11:59:25 +0200

    ✨  Add stories for labeledInput component

---
+++ src/app/components/LabeledInput/LabeledInput.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import LabeledInput from "./LabeledInput";
+
+export default {
+  title: "Component/LabeledInput",
+  component: LabeledInput,
+};
+
+export const email = (): JSX.Element => (
+  <LabeledInput
+    label="Email address"
+    // value=""
+    type="email"
+    required={true}
+    onChange={console.log}
+  />
+);
+
+export const password = (): JSX.Element => (
+  <LabeledInput
+    label="Password"
+    // value=""
+    type="password"
+    required={true}
+    onChange={console.log}
+  />
+);
+
+export const firstName = (): JSX.Element => (
+  <LabeledInput
+    label="First name"
+    // value=""
+    type=""
+    required={true}
+    onChange={console.log}
+  />
+);
