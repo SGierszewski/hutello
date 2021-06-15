a8f089859e9d1ad3cc8fcbbd80e378378c31a773
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-15 16:32:43 +0200

    💄  Add global button-icon-color

--- src/app/components/Icons/RequestIcon.tsx
+++ src/app/components/Icons/RequestIcon.tsx
@@ -11,7 +11,7 @@ export default function RequestIcon(): JSX.Element {
       <path
         d="M27.3,3H5.7A2.7,2.7,0,0,0,3.013,5.7L3,30l5.4-5.4H27.3A2.708,2.708,0,0,0,30,21.9V5.7A2.708,2.708,0,0,0,27.3,3ZM24.6,19.2H8.4V16.5H24.6Zm0-4.05H8.4v-2.7H24.6Zm0-4.05H8.4V8.4H24.6Z"
         transform="translate(-3 -3)"
-        fill="#fff"
+        fill="var(--button-icon-color)"
       />
     </svg>
   );
--- src/app/components/Icons/UserIcon.tsx
+++ src/app/components/Icons/UserIcon.tsx
@@ -11,7 +11,7 @@ export default function UserIcon(): JSX.Element {
       <path
         d="M15.76,15.76a4.88,4.88,0,1,0-4.88-4.88A4.878,4.878,0,0,0,15.76,15.76Zm0,2.44C12.5,18.2,6,19.834,6,23.079v2.44H25.519v-2.44C25.519,19.834,19.017,18.2,15.76,18.2Z"
         transform="translate(-6 -6)"
-        fill="#fff"
+        fill="var(--button-icon-color)"
       />
     </svg>
   );
