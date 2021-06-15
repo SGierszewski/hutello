fc68b7ccad52577198b8f4fd42840143c8dd08af
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-15 11:39:05 +0200

    ✨  Create AppLogo story

---
+++ src/app/components/AppLogo/AppLogo.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AppLogo from "./AppLogo";
+
+export default {
+  title: "Component/AppLogo",
+  component: AppLogo,
+};
+
+export const AppLpgo = (): JSX.Element => <AppLogo title={"Hutello"} />;
