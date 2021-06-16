2b285aea6019d70ce9860b043a689f7e7a1bda45
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-16 17:02:05 +0200

    ♻️  Refactor code to include button variants

--- src/app/components/IconButton/IconButton.tsx
+++ src/app/components/IconButton/IconButton.tsx
@@ -1,19 +1,32 @@
 import React from "react";
+import PwResetIcon from "../Icons/PwResetIcon";
+import RequestIcon from "../Icons/RequestIcon";
+import SearchIcon from "../Icons/SearchIcon";
+import UserIcon from "../Icons/UserIcon";
 import styles from "./IconButton.module.css";
 
 type IconButtonProps = {
-  icon: React.SVGProps<SVGSVGElement>;
-  title: string;
+  onClick: () => void;
+  variant: "login" | "signUp" | "pwReset" | "search" | "newSearch" | "request";
+};
+
+const variants = {
+  login: { icon: <UserIcon />, title: "Login" },
+  signUp: { icon: <UserIcon />, title: "Sign up" },
+  pwReset: { icon: <PwResetIcon />, title: "Reset password" },
+  search: { icon: <SearchIcon />, title: "Search" },
+  newSearch: { icon: <SearchIcon />, title: "New search" },
+  request: { icon: <RequestIcon />, title: "Send request" },
 };
 
 export default function IconButton({
-  icon,
-  title,
+  onClick,
+  variant,
 }: IconButtonProps): JSX.Element {
   return (
-    <button className={styles.button}>
-      <span className={styles.button__icon}>{icon}</span>
-      <span className={styles.button__title}>{title}</span>
+    <button className={styles.button} onClick={onClick}>
+      <span className={styles.button__icon}>{variants[variant].icon}</span>
+      <span className={styles.button__title}>{variants[variant].title}</span>
     </button>
   );
 }
