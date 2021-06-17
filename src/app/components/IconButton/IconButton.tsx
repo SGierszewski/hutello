0733525f686b2bcd6195e1a5da797b12be26aa08
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-17 12:59:56 +0200

    ♻️  Refactor code to pass button icon and title separately

--- src/app/components/IconButton/IconButton.tsx
+++ src/app/components/IconButton/IconButton.tsx
@@ -1,32 +1,21 @@
 import React from "react";
-import PwResetIcon from "../Icons/PwResetIcon";
-import RequestIcon from "../Icons/RequestIcon";
-import SearchIcon from "../Icons/SearchIcon";
-import UserIcon from "../Icons/UserIcon";
 import styles from "./IconButton.module.css";
 
 type IconButtonProps = {
   onClick: () => void;
-  variant: "login" | "signUp" | "pwReset" | "search" | "newSearch" | "request";
-};
-
-const variants = {
-  login: { icon: <UserIcon />, title: "Login" },
-  signUp: { icon: <UserIcon />, title: "Sign up" },
-  pwReset: { icon: <PwResetIcon />, title: "Reset password" },
-  search: { icon: <SearchIcon />, title: "Search" },
-  newSearch: { icon: <SearchIcon />, title: "New search" },
-  request: { icon: <RequestIcon />, title: "Send request" },
+  children: React.ReactNode;
+  title: string;
 };
 
 export default function IconButton({
   onClick,
-  variant,
+  children,
+  title,
 }: IconButtonProps): JSX.Element {
   return (
     <button className={styles.button} onClick={onClick}>
-      <span className={styles.button__icon}>{variants[variant].icon}</span>
-      <span className={styles.button__title}>{variants[variant].title}</span>
+      <span className={styles.button__icon}>{children}</span>
+      <span className={styles.button__title}>{title}</span>
     </button>
   );
 }
