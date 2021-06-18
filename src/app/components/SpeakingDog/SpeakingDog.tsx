666687a9f76db60aaf2a2c32f17495efbdd5a45b
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-18 16:29:40 +0200

    ✨  Add SpeakingDog component

---
+++ src/app/components/SpeakingDog/SpeakingDog.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import SpeechBubble from "../Icons/SpeechBubble";
+import LogoIcon from "../Icons/logoIcon";
+import styles from "./SpeakingDog.module.css";
+
+type SpeakingDogProps = {
+  speech: string;
+};
+
+export default function SpeakingDog({ speech }: SpeakingDogProps): JSX.Element {
+  return (
+    <>
+      <div className={styles.bubble}>
+        <SpeechBubble speech={speech} />
+      </div>
+      <div className={styles.dog}>
+        <LogoIcon />
+      </div>
+    </>
+  );
+}
