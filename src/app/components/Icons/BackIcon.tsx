83203991b675915ec64516a91cf288b90b724102
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-18 11:33:24 +0200

    ♻️ Refactor code

--- src/app/components/Icons/BackIcon.tsx
+++ src/app/components/Icons/BackIcon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function BackIcon(): JSX.Element {
+export default function BackIcon(): JSX.Element {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
