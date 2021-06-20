669a3a67fc8efad911a4a09673846b31eff018d7
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-20 18:19:40 +0200

    ✨  Add ForgotPassword page

---
+++ src/app/pages/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import LabeledInput from "../../components/LabeledInput/LabeledInput";
+import IconButton from "../../components/IconButton/IconButton";
+import SpeakingDog from "../../components/SpeakingDog/SpeakingDog";
+import BackButton from "../../components/BackButton/BackButton";
+import PwResetIcon from "../../components/Icons/PwResetIcon";
+import styles from "./ForgotPassword.module.css";
+
+export default function ForgotPassword(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <BackButton />
+      </header>
+      <main className={styles.main}>
+        <div className={styles.main__image}>
+          <SpeakingDog speech="no worries!!" />
+        </div>
+        <h2 className={styles.main__subtitle}>
+          Forgot your password? <br />
+          Just enter your registered email address below and we will send you a
+          link to reset your password.
+        </h2>
+        <form className={styles.main__forgotPwForm}>
+          <LabeledInput
+            label="Email address"
+            value=""
+            type="email"
+            required={true}
+            onChange={() => console.log("clicked")}
+          />
+          <IconButton
+            onClick={() => console.log("clicked")}
+            children={<PwResetIcon />}
+            title="Reset password"
+          />
+        </form>
+      </main>
+    </div>
+  );
+}
