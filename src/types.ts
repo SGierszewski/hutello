5a29bec8d1aaf475fa5898e83c8759a338833f94
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-21 21:02:02 +0200

    ✨  Add types for User and Dog registration

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export type User = {
+  image: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  streetName: string;
+  postcode: number;
+  city: string;
+  docs: string;
+};
+
+export type Dog = {
+  image: string;
+  name: string;
+  sex: string;
+  dateOfBirth: number;
+  breed: string;
+  size: number;
+  weight: number;
+};
