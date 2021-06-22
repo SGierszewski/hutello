6caff06dd90a65da681de560b526543322ae0037
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-22 20:26:35 +0200

    ✨  Add route to Login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,53 +1,29 @@
-import React, { useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter, Switch, Route, RouteProps } from "react-router-dom";
 import styles from "./App.module.css";
+import Login from "./pages/Login/Login";
+
+type CustomRouteProps = RouteProps & {
+  Component: () => JSX.Element;
+  path: string;
+};
+
+const routes: CustomRouteProps[] = [
+  { path: "/", Component: Login, exact: true },
+];
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
   return (
     <div className={styles.App}>
       <BrowserRouter>
         <Switch>
-          <Route path="/help">
-            <p>Help</p>
-          </Route>
-          <Route path="/">
-            <p>Home</p>
-          </Route>
+          {routes.map(({ Component, ...routeProps }) => (
+            <Route key={routeProps.path} {...routeProps}>
+              <Component />
+            </Route>
+          ))}
         </Switch>
       </BrowserRouter>
-      <header className={styles["App-header"]}>
-        <img src={logo} className={styles["App-logo"]} alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className={styles["App-link"]}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className={styles["App-link"]}
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
     </div>
   );
 }
