7820ffb8716dbc416081e2436f48ca38ad8ba2e9
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-23 15:44:27 +0200

    ✨  Add login verification

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,3 +15,19 @@ export async function postUser(user: User): Promise<User> {
   const result: User = await response.json();
   return result;
 }
+
+export async function verifyLogin(user: Partial<User>): Promise<User> {
+  const response = await fetch("/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw errorMessage;
+  }
+  const result: User = await response.json();
+  return result;
+}
