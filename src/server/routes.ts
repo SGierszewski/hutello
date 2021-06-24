8bf8020ea9ec44a2ec60821fe398a04217038347
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-24 21:56:27 +0200

    🚧 Try to implement search routes

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { saveHuta, readHutas } from "./hutas";
+import { saveHuta, readHutas, searchHutas } from "./hutas";
 import { saveUser, readUsers } from "./users";
 
 const router = express.Router();
@@ -22,6 +22,25 @@ router.post("/hutas", async (req, res) => {
 router.get("/hutas", async (_req, res) => {
   const hutas = await readHutas();
   res.json(hutas);
+});
+
+router.get("/hutas/:id", async (_req, res) => {
+  const hutas = await readHutas();
+  res.json(hutas);
+});
+
+router.get("/search", async (req, res, next) => {
+  try {
+    const { query } = req.query;
+    if (typeof query.city !== "string") {
+      res.status(400).send("Keyword is not valid");
+      return;
+    }
+    const searchResult = await searchHutas(query.city);
+    res.status(200).json(searchResult);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
