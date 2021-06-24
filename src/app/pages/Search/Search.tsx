e752f58ac5d2683eeb43ad48b2a108776646656b
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-24 21:54:34 +0200

    🚧 Start implementing functionality to SearchPage

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -1,61 +1,65 @@
 import React, { useState } from "react";
+import { Huta } from "../../../types";
 import IconButton from "../../components/IconButton/IconButton";
 import SearchIcon from "../../components/Icons/SearchIcon";
 import LabeledInput from "../../components/LabeledInput/LabeledInput";
 import SpeakingDog from "../../components/SpeakingDog/SpeakingDog";
 import styles from "./Search.module.css";
-import type { DogsPlaces } from "../../../../src/types";
+
+type Hutas = Huta[];
 
 export default function SearchPage(): JSX.Element {
-  const [dogsPlacesCity, setDogsPlacesCity] = useState<DogsPlaces | null>(null);
+  const [hutas, setHutas] = useState<Hutas | null>(null);
 
-  async function handleSearch(dogsPlacesCity: string) {
-    const response = await fetch(`/api/search?city=${dogsPlacesCity}`);
-    const searchedCity = await response.json();
-    setDogsPlacesCity(searchedCity);
+  async function handleSearch(
+    city: string,
+    event: React.FormEvent<HTMLFormElement>
+  ) {
+    console.log(hutas);
+    event.preventDefault();
+    const response = await fetch(`/api/search?city=${city}`);
+    const searchedHuta = await response.json();
+    setHutas(searchedHuta);
   }
 
-  if (dogsPlacesCity) {
-    return (
-      <SearchResults
-        dogsPlaces={dogsPlaces}
-        onBack={() => setDogsPlacesCity(null)}
-      />
-    );
-  }
+  // if (huta) {
+  //   return <SearchResults huta={huta} onBack={() => setHuta(null)} />;
+  // }
 
   return (
-    <div>
+    <div className={styles.container}>
       <header>
         <SpeakingDog speech="where can i stay?" />
         <h2>Search dog daycare places in…</h2>
       </header>
-      <form className={styles.searchForm} onSearch={handleSearch}>
-        <LabeledInput
-          label="City name"
-          value={dogsPlacesCity}
-          onChange={setDogsPlacesCity}
-        />
+      <form className={styles.searchForm} onSubmit={handleSearch}>
+        <LabeledInput label="City name" value={hutas} onChange={setHutas} />
         <IconButton children={<SearchIcon />} title="Search" />
       </form>
+      <h3>RESULTS</h3>
+      <ul>
+        {hutas?.map((huta) => {
+          return <li>{huta.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
 
-export function SearchResults({ dogsPlaces }: DogsPlaces): JSX.Element {
-  return (
-    <div>
-      <header>
-        <IconButton children={<SearchIcon />} title="New search" />
-        <h2>Search result(s)</h2>
-      </header>
-      <main>
-        <ul>
-          {dogsPlaces.map((dogsPlace) => (
-            <li key={dogsPlace.id}>{dogsPlace.name}</li>
-          ))}
-        </ul>
-      </main>
-    </div>
-  );
-}
+// export function SearchResults(hutas: Huta): JSX.Element {
+//   return (
+//     <div>
+//       <header>
+//         <IconButton children={<SearchIcon />} title="New search" />
+//         <h2>Search result(s)</h2>
+//       </header>
+//       <main>
+//         <ul>
+//           {hutas.map((huta) => (
+//             <li key={huta._id}>{huta.name}</li>
+//           ))}
+//         </ul>
+//       </main>
+//     </div>
+//   );
+// }
