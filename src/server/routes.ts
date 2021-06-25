0e2cea6f02cc9fbeb2f53681a2e4dfb61a2f18d8
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-25 18:30:53 +0200

    ✨ Check if user is logged in

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { saveUser, readUsers, readUser } from "./users";
+import { ObjectID } from "mongodb";
 
 const router = express.Router();
 
@@ -26,6 +27,24 @@ router.post("/users/login", async (req, res, next) => {
       "Set-Cookie",
       `userID=${user._id};path=/;Max-Age=${365 * 24 * 60 * 60}`
     );
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Check user login
+router.get("/users/me", async (req, res, next) => {
+  try {
+    const { userId } = req.cookies;
+    if (!userId) {
+      return res.status(401).end("Access denied! Please login first.");
+    }
+    const user = await readUser({ _id: new ObjectID(userId) });
+    if (!user) {
+      res.status(404).send("User not found.");
+      return;
+    }
     res.status(200).json(user);
   } catch (error) {
     next(error);
