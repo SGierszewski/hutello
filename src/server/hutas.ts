c1447ddd5059aab26a7fd9f216c0b7f42d495ab9
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-25 13:05:48 +0200

    💥  Finish functions to get and filter HuTas from DB

--- src/server/hutas.ts
+++ src/server/hutas.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import type { Huta } from "../types";
 import { getHutasCollection } from "./database";
 
@@ -7,4 +8,15 @@ export const saveHuta = async (huta: Huta): Promise<void> => {
 
 export const readHutas = async (): Promise<Huta[]> => {
   return await getHutasCollection().find().sort({ name: 1 }).toArray();
+};
+
+export const filterHutas = async (city: string): Promise<Huta[]> => {
+  return await getHutasCollection().find({ city }).sort({ name: 1 }).toArray();
+};
+
+export const getHutaById = async (_id: string): Promise<Huta | null> => {
+  const hutaResult = await getHutasCollection().findOne({
+    _id: new ObjectId(_id),
+  });
+  return hutaResult;
 };
