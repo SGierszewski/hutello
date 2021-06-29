6341b42a7f3129dc6a14b571783932a1ef806498
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-29 12:38:51 +0200

    💄  Add fullscreen layout to Register story

--- src/app/pages/Register/Register.stories.tsx
+++ src/app/pages/Register/Register.stories.tsx
@@ -4,6 +4,9 @@ import Register from "./Register";
 export default {
   title: "Page/Register",
   component: Register,
+  parameters: {
+    layout: "fullscreen",
+  },
 };
 
 export const Default = (): JSX.Element => <Register />;
