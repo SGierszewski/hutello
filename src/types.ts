002817835319bf499b97366a99bc30aa17323f5f
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-30 14:46:25 +0200

    🏷️  Add type for CloudinaryResult

--- src/types.ts
+++ src/types.ts
@@ -29,3 +29,25 @@ export type Huta = {
   name: string;
   city: string;
 };
+
+export type CloudinaryResult = {
+  access_mode: string;
+  asset_id: string;
+  bytes: number;
+  created_at: string;
+  etag: string;
+  format: string;
+  height: number;
+  original_filename: string;
+  placeholder: false;
+  public_id: string;
+  resource_type: string;
+  secure_url: string;
+  signature: string;
+  tags: [];
+  type: string;
+  url: string;
+  version: number;
+  version_id: string;
+  width: number;
+};
