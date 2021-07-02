c9efa9d12b68ee3de7099c08d16575a3f365b909
SGierszewski <stefaniegierszewski@hotmail.com>
2021-07-02 22:44:39 +0200

    ✨  Add story for AboutMeCard

---
+++ src/app/components/AboutMeCard/AboutMeCard.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AboutMeCard from "./AboutMeCard";
+
+export default {
+  title: "Component/AboutMeCard",
+  component: AboutMeCard,
+};
+
+export const ProfileCard = (): JSX.Element => <AboutMeCard />;
