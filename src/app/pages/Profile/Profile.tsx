043ad9cebf99ff3c65997d89b08970bd02d7a0ed
SGierszewski <stefaniegierszewski@hotmail.com>
2021-07-02 22:54:37 +0200

    💄  Add styles to Profile page

--- src/app/pages/Profile/Profile.tsx
+++ src/app/pages/Profile/Profile.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import AboutMeCard from "../../components/AboutMeCard/AboutMeCard";
 import Steffi from "/src/app/assets/images/Steffi.jpeg";
 import Susi from "/src/app/assets/images/Susi.png";
+import styles from "./Profile.module.css";
 
 export default function Profile(): JSX.Element {
   return (
-    <AboutMeCard imageSrc={Steffi} documentImageSrc="" dogImageSrc={Susi} />
+    <div className={styles.container}>
+      <AboutMeCard imageSrc={Steffi} documentImageSrc="" dogImageSrc={Susi} />
+    </div>
   );
 }
