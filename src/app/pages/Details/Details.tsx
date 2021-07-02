63035687b3eacddf1609d1f1dd1f08399ebc7194
SGierszewski <stefaniegierszewski@hotmail.com>
2021-07-03 00:03:27 +0200

    ✨  Add Details page

---
+++ src/app/pages/Details/Details.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import BackButton from "../../components/BackButton/BackButton";
+import IconButton from "../../components/IconButton/IconButton";
+import RequestIcon from "../../components/Icons/RequestIcon";
+import styles from "./Details.module.css";
+
+export default function Details(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <BackButton />
+      </header>
+      <article className={styles.hutaDetails__card}>
+        <div className={styles.hutaDetails__header}>
+          //FAVORITE ICON
+          <h3>Dog's Paradise</h3>
+          <img
+            className={styles.hutaDetails__img}
+            src="src/app/assets/images/huta_example1.jpeg"
+            alt=""
+          />
+        </div>
+        <IconButton title="Send request">{<RequestIcon />}</IconButton>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel
+          pharetra vel turpis nunc eget lorem dolor sed viverra. Consectetur
+          purus ut faucibus pulvinar elementum integer enim neque volutpat.
+          Accumsan tortor posuere ac ut. Suspendisse ultrices gravida dictum
+          fusce ut placerat orci nulla. Commodo ullamcorper a lacus vestibulum
+          sed. Vitae proin sagittis nisl rhoncus mattis rhoncus. Tincidunt eget
+          nullam non nisi est sit amet facilisis. Interdum consectetur libero id
+          faucibus nisl. Vel facilisis volutpat est velit egestas dui id ornare.
+          Enim eu turpis egestas pretium aenean pharetra magna ac. Mauris augue
+          neque gravida in fermentum et sollicitudin.
+        </p>
+        <div className={styles.hutaDetails__images}>
+          <img src="src/app/assets/images/huta_example2.jpeg" alt="" />
+          <img src="src/app/assets/images/huta_example2.jpeg" alt="" />
+          <img src="src/app/assets/images/huta_example3.jpeg" alt="" />
+          <img src="src/app/assets/images/huta_example3.jpeg" alt="" />
+        </div>
+        <div className={styles.hutaDetails__contact}>
+          <p>
+            Musterstraße 3<br />
+            50000 Musterstadt
+          </p>
+          <p>0000-123456-789</p>
+          <a href="">huta@email.de </a>
+          <a href="">www.huta.de</a>
+        </div>
+      </article>
+    </div>
+  );
+}
