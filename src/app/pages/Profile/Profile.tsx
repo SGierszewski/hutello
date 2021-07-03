14b0879ba015eb0e5372f7b740a48646c24845b2
SGierszewski <stefaniegierszewski@hotmail.com>
2021-07-03 21:49:56 +0200

    ♻️ Update asset imports for Profile page

--- src/app/pages/Profile/Profile.tsx
+++ src/app/pages/Profile/Profile.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import AboutMeCard from "../../components/AboutMeCard/AboutMeCard";
-import Steffi from "/src/app/assets/images/Steffi.jpeg";
-import Susi from "/src/app/assets/images/Susi.png";
+import Steffi from "/src/app/assets/images/steffi.png";
+import Susi from "/src/app/assets/images/susi.png";
+import imgUpload from "/src/app/assets/images/imgUpload.png";
 import styles from "./Profile.module.css";
 
 export default function Profile(): JSX.Element {
   return (
     <div className={styles.container}>
-      <AboutMeCard imageSrc={Steffi} documentImageSrc="" dogImageSrc={Susi} />
+      <AboutMeCard
+        imageSrc={Steffi}
+        documentImageSrc={imgUpload}
+        dogImageSrc={Susi}
+      />
     </div>
   );
 }
