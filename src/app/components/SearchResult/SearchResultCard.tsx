26f783cdb6588273ea5904dd2614b195adf46b60
SGierszewski <stefaniegierszewski@hotmail.com>
2021-07-03 10:49:43 +0200

    ✨ Add SearchResultCard component

--- src/app/components/SearchResult/SearchResultCard.tsx
+++ src/app/components/SearchResult/SearchResultCard.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import NavbarFavoriteIcon from "../Icons/NavbarFavoriteIcon";
-import styles from "/SearchResultCard.module.css";
+import FavoriteIconLink from "../FavoriteIconLink/FavoriteIconLink";
+import FavoriteIcon from "../Icons/FavoriteIcon";
+import styles from "./SearchResultCard.module.css";
 
 export default function SearchResultCard(): JSX.Element {
   return (
     <article className={styles.searchResult__card}>
-      <h3>Dog's paradise</h3>
-      <p>Musterstraße 3</p>
-      <p>50677 Köln</p>
+      <div className={styles.searchResult__card_info}>
+        <h3>Dog's paradise</h3>
+        <p>Musterstraße 3</p>
+        <p>50677 Köln</p>
+      </div>
       <div className={styles.searchResult__favoriteIcon}>
-        <NavbarFavoriteIcon />
+        <FavoriteIconLink link="#" icon={<FavoriteIcon />} active={false} />
       </div>
     </article>
   );
