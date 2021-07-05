b621f54594f0e3f57852e1fd888e7b9af024d32d
SGierszewski <stefaniegierszewski@hotmail.com>
2021-07-05 13:32:42 +0200

    🐛  Update asset import to fix bug

--- src/app/pages/Profile/Profile.tsx
+++ src/app/pages/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AboutMeCard from "../../components/AboutMeCard/AboutMeCard";
 import Steffi from "/src/app/assets/images/steffi.png";
-import Susi from "src/app/assets/images/susi.png";
+import susiTheDog from "/src/app/assets/images/susiTheDog.png";
 import imgUpload from "/src/app/assets/images/imgUpload.png";
 import styles from "./Profile.module.css";
 
@@ -11,7 +11,7 @@ export default function Profile(): JSX.Element {
       <AboutMeCard
         imageSrc={Steffi}
         documentImageSrc={imgUpload}
-        dogImageSrc={Susi}
+        dogImageSrc={susiTheDog}
       />
     </div>
   );
